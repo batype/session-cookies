405491eacf192e1a8f7d3f8b9fd62572418aa134
ss01160899 <ss01160899@alibaba-inc.com>
2023-08-03 16:51:01 +0800

    update export

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import * as local from "./LocalStorage";
 import * as session from "./SessionStorage";
 import * as cookie from "./Cookie";
 
-export default { local, session, cookie };
+export { local, session, cookie };
